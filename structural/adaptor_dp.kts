07ec6b89310fa8c573b3d376ea1f08eaa6a89305
Ranjith P <pranjith0869@gmail.com>
2025-01-25 23:10:07 +0530

    Update adaptor_dp.kts

--- structural/adaptor_dp.kts
+++ structural/adaptor_dp.kts
@@ -1,1 +1,55 @@
+package com.ranjith.payments
 
+interface OfflinePayment {
+    fun acceptPayment()
+}
+
+package com.ranjith.payments
+
+class CashPayment(private val amountToPay: Double): OfflinePayment {
+    override fun acceptPayment() {
+        println("Accepting $amountToPay cash payment")
+    }
+}
+
+package com.ranjith.payments
+
+interface DigitalPayment {
+    fun acceptPaymentDigitally()
+}
+
+package com.ranjith.payments
+
+class CryptoPayment(private val amountToPay: String): DigitalPayment {
+    override fun acceptPaymentDigitally() {
+        println("Accepting $amountToPay crypto payment")
+    }
+}
+
+package com.ranjith.payments
+
+class DigitalPaymentAdapter(private val digitalPayment: DigitalPayment): OfflinePayment {
+    override fun acceptPayment() {
+        digitalPayment.acceptPaymentDigitally()
+    }
+}
+
+//Client
+package com.ranjith.service
+
+import com.ranjith.payments.CashPayment
+import com.ranjith.payments.CryptoPayment
+import com.ranjith.payments.DigitalPaymentAdapter
+
+class PaymentService {
+    fun payBill() {
+        val paymentTypeObject = CashPayment(50.0)
+        paymentTypeObject.acceptPayment()
+
+        val digitalPaymentObject = CryptoPayment("45.0")
+        //digitalPaymentObject.acceptPayment() -> Can't be used as it is not compatible
+
+        val digitalPaymentAdapter = DigitalPaymentAdapter(CryptoPayment("45.0"))
+        digitalPaymentAdapter.acceptPayment()
+    }
+}
